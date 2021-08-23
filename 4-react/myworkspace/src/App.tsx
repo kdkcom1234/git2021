85130340de753c076496f20341b35a51126088de
Daekeun Ko <kdkcom1234@gmail.com>
2021-08-23 16:09:23 +0900

    app

--- 4-react/myworkspace/src/App.tsx
+++ 4-react/myworkspace/src/App.tsx
@@ -4,11 +4,10 @@
 // css를 사용하는 컴포넌트 범위로 css class 사용범위를 좁힐 수 있음.
 
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
 import Home from "./components/Home";
-import Navigation from "./Navigation";
 
 // SPA(Single Page Application)
 // : 페이지 파일이 1개, index.html
@@ -18,12 +17,6 @@ import Navigation from "./Navigation";
 
 // Lazy-Loading 처리
 // 컴포넌트를 방문하는 시점에 로딩함
-const Counter = lazy(() => import("./components/Counter"));
-const Calculator = lazy(() => import("./components/CalculatorRef"));
-const Generator = lazy(() => import("./components/Generator"));
-const AccountManager = lazy(() => import("./components/AccountManagerRef"));
-const Components = lazy(() => import("./components/Components"));
-const BootStrap = lazy(() => import("./components/Bootstrap"));
 const Todo = lazy(() => import("./components/Todo"));
 const Feed = lazy(() => import("./components/Feed"));
 
@@ -37,7 +30,17 @@ function App() {
           style={{ width: "200px", height: "100vh", top: "20px" }}
           className="position-fixed"
         >
-          <Navigation />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo</Link>
+            </li>
+            <li>
+              <Link to="/feeds">Feeds</Link>
+            </li>
+          </ul>
         </nav>
         <main style={{ marginLeft: "200px", marginTop: "20px" }}>
           {/* Suspense 컴포넌트로 로딩중에 보여줄 화면을 처리하는 것 */}
@@ -48,12 +51,6 @@ function App() {
 
               {/* 해당 경로에 대해서 로딩할 컴포넌트 목록을 작성 */}
               <Route path="/" component={Home} exact />
-              <Route path="/components" component={Components} />
-              <Route path="/counter" component={Counter} />
-              <Route path="/calculator" component={Calculator} />
-              <Route path="/generator" component={Generator} />
-              <Route path="/account-manager" component={AccountManager} />
-              <Route path="/bootstrap" component={BootStrap} />
               <Route path="/todo" component={Todo} />
               <Route path="/feeds" component={Feed} />
             </Switch>
