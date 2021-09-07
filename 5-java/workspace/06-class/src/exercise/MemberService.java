6202b45e1bd105ab3952f2865df781703646f987
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-07 15:47:24 +0900

    usaage, sequence diagram

--- 5-java/workspace/06-class/src/exercise/MemberService.java
+++ 5-java/workspace/06-class/src/exercise/MemberService.java
@@ -1,13 +1,24 @@
 package exercise;
 
 public class MemberService {
-	boolean login(String id, String password) {
-		if (id == "hong" && password == "12345") {
+
+	// Member 객체를 메서드 파라미터로 받음
+	// Dependency: 의존성이 있음
+	// Dependency<<Usage>>: 구현이나 기능처리에 다른 클래스가 필요함
+	boolean login(Member member) {
+		if (member.id == "hong" && member.password == "12345") {
 			return true;
-		} else {
-			return false;
 		}
+		return false;
 	}
+
+//	boolean login(String id, String password) {
+//		if (id == "hong" && password == "12345") {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
 
 	void logout(String id) {
 		System.out.println("로그아웃 되었습니다");
--- 5-java/workspace/06-class/src/exercise/MemberServiceExample.java
+++ 5-java/workspace/06-class/src/exercise/MemberServiceExample.java
@@ -5,13 +5,17 @@ public class MemberServiceExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MemberService memberSerivce = new MemberService();
+
 		// 로그인 성공 케이스
-		boolean result = memberSerivce.login("hong", "12345");
-		// 로그인 실패 케이스
-//		boolean result = memberSerivce.login("hong", "1234");
-//		boolean result = memberSerivce.login("hon", "12345");
+		// 테스트 데이터 생성
+		Member member = new Member("홍길동", "hong");
+		member.password = "12345";
+
+		// 로그인 테스트 케이스 실행
+		boolean result = memberSerivce.login(member);
 		if (result) {
 			System.out.println("로그인 되었습니다.");
+			// 로그아웃 테스트 케이스 실행
 			memberSerivce.logout("hong");
 		} else {
 			System.out.println("id 또는 password가 올바르지 않습니다.");
