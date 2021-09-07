1ac23b36169e223b791f9e2764629dcbe111b5bc
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-07 09:42:06 +0900

    20210907 6장 확인문제

--- 5-java/workspace/05-reference-type/src/exercise/Exercise09.java
+++ 5-java/workspace/05-reference-type/src/exercise/Exercise09.java
@@ -23,17 +23,34 @@ public class Exercise09 {
 
 			switch (selectNo) {
 			case 1:
+				System.out.println("학생수> ");
 				// 학생수를 입력받음
 				// 입력한 학생수 만큼 배열크기를 초기화
+				scores = new int[scanner.nextInt()];
 				break;
 			case 2:
 				// 배열크기만큼 반복해서 점수를 입력 받음
+				for (int i = 0; i < scores.length; i++) {
+					System.out.print("scores[" + i + "]> ");
+					scores[i] = scanner.nextInt();
+				}
 				break;
 			case 3:
 				// 배열크기만큼 반복해서 점수 목록을 출력
+				for (int i = 0; i < scores.length; i++) {
+					System.out.println("scores[" + i + "]: " + scores[i]);
+				}
 				break;
 			case 4:
+				int sum = 0;
+				int max = 0;
+				for (int score : scores) {
+					sum += score;
+					max = score > max ? score : max;
+				}
 				// 최고점수와 평균점수 출력
+				System.out.println("최고 점수: " + max);
+				System.out.println("평균 점수: " + sum * 1.0 / scores.length);
 				break;
 			case 5:
 				// run false로 반복문 탈출
