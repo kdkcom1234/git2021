69ff71ccffc31ab3fff6578805e6b9da2aa69e2f
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-08 15:47:24 +0900

    Member Class

--- 5-java/workspace/15-collection/src/map/Member.java
+++ 5-java/workspace/15-collection/src/map/Member.java
@@ -6,6 +6,13 @@ public class Member {
 	private String password;
 	private int age;
 
+	public Member(String name, String id, String password, int age) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.age = age;
+	}
+
 	public String getName() {
 		return name;
 	}
---
+++ 5-java/workspace/15-collection/src/map/MemberExample.java
@@ -0,0 +1,17 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		// Map<키타입, 값타입>
+		// 매개변수로 넣어주는 타입: 제네릭, 제너릭, generic
+		// 키: Member의 id
+		// 값: Member 객체
+		Map<String, Member> members = new HashMap<String, Member>();
+
+	}
+
+}
