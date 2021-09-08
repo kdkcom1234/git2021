1808a33bd75badbb8bacc4f4586cd79e4003bb09
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-08 15:42:46 +0900

    Member Class

---
+++ 5-java/workspace/15-collection/src/map/Member.java
@@ -0,0 +1,40 @@
+package map;
+
+public class Member {
+	private String name;
+	private String id;
+	private String password;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
