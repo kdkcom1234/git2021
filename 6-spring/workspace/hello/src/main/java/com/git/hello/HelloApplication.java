b4c27c1a9530cca4a9cb68375b248cfe74e7d10d
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-13 11:43:50 +0900

    커멘트 수정

--- 6-spring/workspace/hello/src/main/java/com/git/hello/HelloApplication.java
+++ 6-spring/workspace/hello/src/main/java/com/git/hello/HelloApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootConfiguration: 의존성 주입을 할 수 있도록함. 객체관리자를 생성(IoC 컨테이너)
 //@EnableAutoConfiguration: 사용하는 의존성에 따라서 자동으로 환경을 구성함
-//  -> spring-boot-starter-web: embed Tomcat 웹서버를 구동함, 8080포트 응답대기
+//  -> spring-boot-starter-web: 
+//       -> embeded Tomcat 웹서버를 구동함, 8080포트 응답대기
+//       -> Dispatcher Servlet 객체를 생성함
 //  -> spring-boot-devtools: 코드를 고치면 서버를 다시 구동해줌
 //@ComponentScan: 컴포넌트들을 검색하여(main클래스 동위/하위 패키들에서) 싱글턴으로 객체생성을 함
 //  -> Spring Framework에서 컴포넌트(예-@Controller) 어노테이션이 있는 클래스들을 검색함
--- 6-spring/workspace/hello/src/main/java/com/git/hello/HelloController.java
+++ 6-spring/workspace/hello/src/main/java/com/git/hello/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+// Dispatcher Servlet이 HTTP 요청을 받고 
+// HTTP 요청 정보에 맞는 컨트롤러의 메서드를 실행함
+
 // Controller: HTTP 요청에 대한 응답을 처리할 수 있는 클래스
 // RestController: Controller인데 응답으로 데이터 객체만 처리함
 //@Controller
