6693c745944eb0f98c4e707774ae4748439cb919
Daekeun Ko <kdkcom1234@gmail.com>
2021-09-16 13:19:55 +0900

    photo api 추가

---
+++ 4-react/myworkspace/src/features/photo/photoApi.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+// 서버로 부터 받아오는 데이터 1건에 대한 타입
+interface PhotoItemResponse {
+  id: number;
+  title: string;
+  description: string;
+  photoUrl: string;
+  fileType: string;
+  fileName: string;
+  createdTime: number;
+}
+
+interface PhotoItemRequest {
+  title: string;
+  description?: string;
+  photoUrl: string;
+  fileType: string;
+  fileName: string;
+}
+
+// 서버하고 데이터 연동하는 api처리 목록을 별도의 객체로 작성
+// process.env.변수명
+const photoApi = {
+  // axios.get<응답데이터의타입>(요청URL);
+  // GET 요청URL HTTP/1.1
+  fetch: () =>
+    axios.get<PhotoItemResponse[]>(`${process.env.REACT_APP_API_BASE}/photos`),
+
+  // axios.post<응답타입>(요청URL, 요청객체(JSON바디));
+  // POST 요청URL HTTP/1.1  {...}
+  add: (photoItem: PhotoItemRequest) =>
+    axios.post<PhotoItemResponse>(
+      `${process.env.REACT_APP_API_BASE}/photos`,
+      photoItem
+    ),
+  // axios.delete<응답타입>(요청URL);
+  // DELETE 요청URL HTTP/1.1
+  remove: (id: number) =>
+    axios.delete<boolean>(`${process.env.REACT_APP_API_BASE}/photos/${id}`),
+
+  // axios.PUT<응답타입>(요청URL, 요청객체(JSON바디));
+  // PUT 요청URL HTTP/1.1  {...}
+  modify: (id: number, photoItem: PhotoItemRequest) =>
+    axios.put<PhotoItemResponse>(
+      `${process.env.REACT_APP_API_BASE}/photos/${id}`,
+      photoItem
+    ),
+};
+
+export default photoApi;
