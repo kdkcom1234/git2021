e39b5cdd9ecb39a87f3874ecb0f2e866c70aeeb3
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-12 13:24:49 +0900

    discovery client, gateway

---
+++ 8-msa/workspace/service3/src/main/java/com/git/service3/Service3Application.java
@@ -0,0 +1,13 @@
+package com.git.service3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service3Application.class, args);
+	}
+
+}
---
+++ 8-msa/workspace/service3/src/test/java/com/git/service3/Service3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.service3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
