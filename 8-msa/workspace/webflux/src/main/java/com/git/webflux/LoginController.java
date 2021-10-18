58d7486b5e1a38a8b874728741922ce23c27ae0d
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-18 12:48:34 +0900

    의존성 주입 수정

--- 8-msa/workspace/webflux/src/main/java/com/git/webflux/LoginController.java
+++ 8-msa/workspace/webflux/src/main/java/com/git/webflux/LoginController.java
@@ -1,32 +1,28 @@
 package com.git.webflux;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class LoginController {
 
-	private static LoginReposiory repo;
+	R2dbcEntityTemplate db;
 
-	@Autowired
-	public LoginController(LoginReposiory repo) {
-		this.repo = repo;
+	public LoginController(R2dbcEntityTemplate db) {
+		this.db = db;
 	}
-
+	
+	
 	@GetMapping(value = "/test")
 	public Mono<Login> test() {
-		R2dbcEntityTemplate template = new R2dbcEntityTemplate(
-				new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder()
-						.host("database-1.cgp5lxuznsxk.ap-northeast-2.rds.amazonaws.com").port(5432)
-						.database("postgres").schema("auth").username("postgres").password("password123!").build()));
-
-		return template.insert(Login.class)
-				.using(Login.builder().userId("kdkcom").passwd("1234").sessionId("1").build());
+		return db.insert(Login.class)
+				.using(Login.builder()
+						.userId("kdkcom")
+						.passwd("1234")
+						.sessionId("1")
+						.build());
 	}
 }
--- 8-msa/workspace/webflux/src/main/java/com/git/webflux/LoginReposiory.java
+++
@@ -1,9 +0,0 @@
-package com.git.webflux;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginReposiory extends ReactiveCrudRepository<Login, Long> {
-
-}
