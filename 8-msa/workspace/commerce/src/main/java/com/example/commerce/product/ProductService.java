6083ce5c2086ac3eb181eaf76d164d9d956bf268
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-19 14:35:47 +0900

    샘플

--- 8-msa/workspace/commerce/src/main/java/com/example/commerce/product/ProductService.java
+++ 8-msa/workspace/commerce/src/main/java/com/example/commerce/product/ProductService.java
@@ -11,16 +11,18 @@ public class ProductService {
 	@RabbitListener(queues = "sales.product.create")
 	public void receiveSalesProduct(SalesProduct salesProduct) {
 		System.out.println(salesProduct);
-
-		saveProduct(salesProduct);
+		
+		// 미작성 부분
+//		saveProduct(salesProduct);
 	}
 
-	public Product saveProduct(SalesProduct salesProduct) {
-		Product product = Product.builder().id(1).productCode(salesProduct.getCode())
-				.productName(salesProduct.getName()).salesProductId(salesProduct.getId()).build();
-
-		// repo.save(product);
-
-		return product;
-	}
+	// 미작성 부분
+//	public Product saveProduct(SalesProduct salesProduct) {
+//		Product product = Product.builder().id(1).productCode(salesProduct.getCode())
+//				.productName(salesProduct.getName()).salesProductId(salesProduct.getId()).build();
+//
+//		// repo.save(product);
+//
+//		return product;
+//	}
 }
