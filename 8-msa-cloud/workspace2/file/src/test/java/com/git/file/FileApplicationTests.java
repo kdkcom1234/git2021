190747b19996bd73538768e1348681a6c9d8176b
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-22 10:28:29 +0900

    remove

---
+++ 8-msa-cloud/workspace2/file/src/test/java/com/git/file/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
