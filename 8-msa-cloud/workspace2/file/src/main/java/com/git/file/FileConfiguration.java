e496d65e90a64d0d4495f17886ace24f8260e706
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-22 13:55:25 +0900

    access info file

--- 8-msa-cloud/workspace2/file/src/main/java/com/git/file/FileConfiguration.java
+++ 8-msa-cloud/workspace2/file/src/main/java/com/git/file/FileConfiguration.java
@@ -6,23 +6,25 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @Configuration
 public class FileConfiguration {
-
-    @Value("${aws.accessId}")
-    private String accessId;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
     // S3에 접속하는 클라언트를 싱글턴을 생성
     // Spring에서 의존 주입해주는 객체로 사용하겠다.
     @Bean
-    public AmazonS3 getS3Client() {
+    public AmazonS3 getS3Client() throws IOException {
+        ClassPathResource resource = new ClassPathResource("aws-access-info.txt");
+
+        String str = new String(resource.getInputStream().readAllBytes());
+
+        String accessId = str.split(" ")[0];
+        String secretKey = str.split(" ")[1];
+
         AWSCredentials credentials = new BasicAWSCredentials(accessId, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
