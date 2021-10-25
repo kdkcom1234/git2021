f2b5ca3831a83893af347efa0acd79ac73a173ae
Daekeun Ko <kdkcom1234@gmail.com>
2021-10-25 14:29:59 +0900

    commerce jpa구성

--- 8-msa-cloud/workspace/commerce/src/main/java/com/example/commerce/product/Product.java
+++ 8-msa-cloud/workspace/commerce/src/main/java/com/example/commerce/product/Product.java
@@ -1,5 +1,10 @@
 package com.example.commerce.product;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +15,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 //엔터티 모델
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private int id; // 자동증가값
 	private String productCode;
 	private String productName;
