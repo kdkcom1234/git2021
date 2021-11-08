f868bf87c8eb3c3df9f44c923b23d80ffe1a8722
Daekeun Ko <kdkcom1234@gmail.com>
2021-11-08 13:52:53 +0900

    cache evict

--- 8-msa-cloud/workspace/commerce/src/main/java/com/example/commerce/product/ProductService.java
+++ 8-msa-cloud/workspace/commerce/src/main/java/com/example/commerce/product/ProductService.java
@@ -1,28 +1,38 @@
 package com.example.commerce.product;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import com.example.commerce.product.event.SalesProduct;
 
 @Service
 public class ProductService {
+	
+	ProductRepository repo;
+	
+	public ProductService(ProductRepository repo) {
+		this.repo = repo;
+	}
 
+	// 데이터가 갱신되는 시점에 캐시 삭제
+	@CacheEvict(key="products", allEntries = true)
 	@RabbitListener(queues = "sales.product.create")
 	public void receiveSalesProduct(SalesProduct salesProduct) {
 		System.out.println(salesProduct);
-		
-		// 미작성 부분
-//		saveProduct(salesProduct);
+		saveProduct(salesProduct);
 	}
 
 	// 미작성 부분
-//	public Product saveProduct(SalesProduct salesProduct) {
-//		Product product = Product.builder().id(1).productCode(salesProduct.getCode())
-//				.productName(salesProduct.getName()).salesProductId(salesProduct.getId()).build();
-//
-//		// repo.save(product);
-//
-//		return product;
-//	}
+	public Product saveProduct(SalesProduct salesProduct) {
+		Product product = Product
+							.builder()
+							.productCode(salesProduct.getCode())
+							.productName(salesProduct.getName())
+							.salesProductId(salesProduct.getId())
+							.build();
+		repo.save(product);
+
+		return product;
+	}
 }
